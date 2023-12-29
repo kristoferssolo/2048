79c3bee2a3527d44fb3f6d9adc1f04b5a3ce141b
Kristofers Solo <kristians.cagulis@pm.me>
2023-12-29 16:56:28 +0200

    feat(game): add `__mult__` and `__imult__` methods for `Direction`
    Returns tuple of values that were multiplied by a constant

--- src/py2048/utils.py
+++ src/py2048/utils.py
@@ -6,3 +6,11 @@ class Direction(Enum):
     DOWN = (0, 1)
     LEFT = (-1, 0)
     RIGHT = (1, 0)
+
+    def __mul__(self, num: int) -> tuple[int, int]:
+        """Multiply the direction by a constant."""
+        return self.value[0] * num, self.value[1] * num
+
+    def __imul__(self, num: int) -> tuple[int, int]:
+        """Multiply the direction by a constant."""
+        return self.value[0] * num, self.value[1] * num
