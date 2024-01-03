2d218aa4491f03f7411a0d9a2d6732ced67b4f85
Kristofers Solo <kristians.cagulis@pm.me>
2024-01-03 04:25:06 +0200

    update log zip file size

--- src/py2048/utils/__init__.py
+++ src/py2048/utils/__init__.py
@@ -11,7 +11,7 @@ def setup_logger() -> None:
         BASE_PATH.joinpath(".logs", "game.log"),
         format="{time} | {level} | {message}",
         level="DEBUG" if BASE_PATH.joinpath("debug").exists() else "INFO",
-        rotation="1 MB",
+        rotation="10 MB",
         compression="zip",
     )
 
